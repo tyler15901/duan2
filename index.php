ba7ec3004d753b1ac7f0b93c3c9e61c3ec09f5b6
tyler15901 <quangthai15901@gmail.com>
2025-06-06 23:13:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once "includes/header.php";
+?>
+<h2 class="text-center mt-5">Chào mừng đến với Nền tảng Phân tích CV AI!</h2>
+<div class="row mt-4 text-center">
+    <div class="col-md-6">
+        <h4>Ứng viên</h4>
+        <a href="/files/auth/login.php" class="btn btn-primary">Đăng nhập / Đăng ký</a>
+    </div>
+    <div class="col-md-6">
+        <h4>Nhà tuyển dụng</h4>
+        <a href="/files/auth/login.php" class="btn btn-success">Đăng nhập / Đăng ký</a>
+    </div>
+</div>
+<?php require_once "includes/footer.php"; ?>
